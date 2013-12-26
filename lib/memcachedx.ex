5d39b721e34ac229c7586c0328327a7dece0d6e5
Thiago Fernandes Massa <tfmassa@sp.r7.com>
2013-12-26 17:23:17 -0200

    Basic tcp connection

--- lib/memcachedx.ex
+++ lib/memcachedx.ex
@@ -1,2 +1,10 @@
 defmodule Memcachedx do
+  def connect_and_send do
+    host = 'localhost'
+    port = 11211
+    {ok, socket} = :gen_tcp.connect(host, port, [], 1)
+
+    ok = :gen_tcp.send(socket, "get mykey")
+    ok = :gen_tcp.close(socket)
+  end
 end
