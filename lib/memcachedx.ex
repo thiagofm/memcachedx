811dbfa53b2d7dda71a1a8005f2479f76864c102
Thiago Fernandes Massa <tfmassa@sp.r7.com>
2014-03-27 15:25:59 -0300

    Added code that connects to memcached and sends a recognized binary packet

--- lib/memcachedx.ex
+++ lib/memcachedx.ex
@@ -7,4 +7,13 @@ defmodule Memcachedx do
     ok = :gen_tcp.send(socket, "get mykey")
     ok = :gen_tcp.close(socket)
   end
+
+  def connect_and_send_binary do
+    host = 'localhost'
+    port = 11211
+    {ok, socket} = :gen_tcp.connect(host, port, [:binary, {:packet, 0}])
+
+    ok = :gen_tcp.send(socket, <<0x80, 0x00>>)
+    ok = :gen_tcp.close(socket)
+  end
 end
--- test/memcachedx_test.exs
+++ test/memcachedx_test.exs
@@ -1,3 +1,7 @@
 defmodule MemcachedxTest do
   use ExUnit.Case
+
+  test :connect_and_send_binary do
+    Memcachedx.connect_and_send_binary
+  end
 end
