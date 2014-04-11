637c6a45638bbd604221181a70529298cc3b48a4
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-11 11:36:27 -0300

    Added binary request for add

--- lib/packet/builder.ex
+++ lib/packet/builder.ex
@@ -41,7 +41,7 @@ defmodule Memcachedx.Packet.Builder do
   MUST have key.
   MAY have value.
   """
-  def request([opcode, options]) when opcode == :add do
+  def request([opcode, options]) when opcode in [:add, :set] do
     vars = [:opaque, :cas, :key, :value, :extras, :flags, :expiry]
     options = Memcachedx.Utils.Options.initialize_vars(options, vars)
 
