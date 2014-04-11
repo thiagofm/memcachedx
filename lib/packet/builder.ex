856f1f5b25688d94667ec6cdb106fc0fb62d5851
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-11 18:54:58 -0300

    Added version

--- lib/packet/builder.ex
+++ lib/packet/builder.ex
@@ -125,4 +125,19 @@ defmodule Memcachedx.Packet.Builder do
 
     Memcachedx.Packet.Header.merge_header(opcode, options) <> Memcachedx.Packet.Body.merge_body(options)
   end
+
+  @doc """
+  Builds a binary request for a version
+
+  Request:
+
+  MUST NOT have extras.
+  MUST NOT have key.
+  MUST NOT have value.
+  """
+  def request([opcode, options]) when opcode in [:version] do
+    options = Memcachedx.Utils.Options.initialize_vars(options)
+
+    Memcachedx.Packet.Header.merge_header(opcode, options) <> Memcachedx.Packet.Body.merge_body(options)
+  end
 end
