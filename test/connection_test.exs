81f5e08b79a134bb7d5ce69d21cf6c4a2a3bca36
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-13 19:16:49 -0300

    Added connection back, this time to rock and roll

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -1,35 +1,35 @@
 defmodule Memcachedx.ConnectionTest do
   use ExUnit.Case
 
-  #setup do
-    #Memcachedx.TestHelper.memcached_up
-    #:ok
-  #end
+  setup do
+    Memcachedx.TestHelper.memcached_up
+    :ok
+  end
 
-  #teardown do
-    #Memcachedx.TestHelper.memcached_down
-    #:ok
-  #end
+  teardown do
+    Memcachedx.TestHelper.memcached_down
+    :ok
+  end
 
-  #test :start_link do
-    #{:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    #assert pid != nil
-  #end
+  test :start_link do
+    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    assert pid != nil
+  end
 
-  #test :stop do
-    #{:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    #assert Memcachedx.Connection.stop(pid) == :ok
-  #end
+  test :stop do
+    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    assert Memcachedx.Connection.stop(pid) == :ok
+  end
 
-  #test :tcp_closed do
-    #{:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    #Memcachedx.TestHelper.memcached_down
+  test :tcp_closed do
+    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    Memcachedx.TestHelper.memcached_down
 
-    #assert { :noproc, _ } = catch_exit( Memcachedx.Connection.stop(pid) )
-  #end
+    assert { :noproc, _ } = catch_exit( Memcachedx.Connection.stop(pid) )
+  end
 
-  #test :set do
+  test :set do
     #{:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
     #assert Memcachedx.Connection.set(pid, "Hello", "World") == {:ok, :stored}
-  #end
+  end
 end
