f73ddc7b105469abdf6dceed101f936a6f632d8c
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-16 23:26:27 -0300

    Added more test cases

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -28,8 +28,13 @@ defmodule Memcachedx.ConnectionTest do
     assert { :noproc, _ } = catch_exit( Memcachedx.Connection.stop(pid) )
   end
 
-  test :add do
+  test 'add from doc example' do
     {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
     assert Memcachedx.Connection.run(pid, [:add, [key: "Hello", value: "World", cas: 0, flags: 0xdeadbeef, expiry: 0x00000e10, extras: 8]]) == {:ok, [cas: 1]}
+  end
+
+  test 'add' do
+    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    assert Memcachedx.Connection.run(pid, [:add, [key: "Hello", value: "World", flags: 0, expiry: 0, extras: 8]]) == {:ok, [cas: 1]}
   end
 end
