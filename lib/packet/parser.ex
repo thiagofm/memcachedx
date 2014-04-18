c81d97cefd55ac0922623a78f286e42603d1f4e9
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-18 20:58:21 -0300

    Added opcode

--- lib/packet/parser.ex
+++ lib/packet/parser.ex
@@ -3,6 +3,10 @@ defmodule Memcachedx.Packet.Parser do
   Parses the response to find the status code and return a friendly message
   based on it.
   """
+  def response(message) do
+    { status(message), params(message)}
+  end
+
   def status(message) do
     status = slice_and_sum(message, 6, 2)
     case status do
@@ -11,18 +15,54 @@ defmodule Memcachedx.Packet.Parser do
     end
   end
 
-  def params(message) do
-    params = [cas: slice_and_sum(message, 16,8)]
+  def opcode(message, params) do
+    case Enum.at(message, 1) do
+      0x00 -> :get
+      0x01 -> :set
+      0x02 -> :add
+      0x03 -> :replace
+      0x04 -> :delete
+      0x05 -> :incr
+      0x06 -> :decr
+      0x07 -> :quit
+      0x08 -> :flush
+      0x0A -> :noop
+      0x0B -> :version
+      0x0D -> :getkq
+      0x0E -> :append
+      0x0F -> :prepend
+      0x10 -> :stat
+      0x11 -> :setq
+      0x12 -> :addq
+      0x13 -> :replaceq
+      0x14 -> :deleteq
+      0x15 -> :incrq
+      0x16 -> :decrq
+    end
+  end
 
+  def total_body(message, params) do
     if slice_and_sum(message, 8, 4) > 0 do
       params = params ++ [total_body: Enum.slice(message, 8, 4) |> Enum.reduce(&+/2)]
     end
+    params
+  end
+
+  def params(message) do
+    params = [cas: slice_and_sum(message, 16,8)]
+    params = total_body(message, params)
 
     params
   end
 
-  def response(message) do
-    { status(message), params(message)}
+  @doc """
+  Returns the necessary extra vars for the defined opcode
+  """
+  def extra_vars_for(opcode) do
+    case opcode do
+      opcode when opcode in [:incr, :decr] -> [:value]
+      _ -> []
+    end
   end
 
   defp slice_and_sum(message, from, to) do
--- test/packet/parser_test.exs
+++ test/packet/parser_test.exs
@@ -4,4 +4,36 @@ defmodule Memcachedx.Packet.ParserTest do
   test 'response set' do
     assert Memcachedx.Packet.Parser.response([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == {:ok, [cas: 1]}
   end
+
+  test 'status ok' do
+    assert Memcachedx.Packet.Parser.status([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :ok
+  end
+
+  test 'status error' do
+    assert Memcachedx.Packet.Parser.status([129, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :error
+  end
+
+  test :total_body do
+    assert Memcachedx.Packet.Parser.total_body([
+      0,0,0,0,
+      0,0,0,0,
+      0,0,0,0
+    ], []) == []
+
+    assert Memcachedx.Packet.Parser.total_body([
+      0,0,0,0,
+      0,0,0,0,
+      0,0,0,1
+    ], []) == [total_body: 1]
+  end
+
+  test :extra_vars_for do
+    assert Memcachedx.Packet.Parser.extra_vars_for(:incr) == [:value]
+    assert Memcachedx.Packet.Parser.extra_vars_for(:decr) == [:value]
+  end
+
+  test :opcode do
+    assert Memcachedx.Packet.Parser.opcode([0, 0x01], []) == :set
+    assert Memcachedx.Packet.Parser.opcode([0, 0x16], []) == :decrq
+  end
 end
