b0f0ed87d314047b3cdf916bab9244c8cd9dee88
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-19 18:52:38 -0300

    Some code improvements

--- lib/packet/response/header.ex
+++ lib/packet/response/header.ex
@@ -11,10 +11,6 @@ defmodule Memcachedx.Packet.Response.Header do
     end
   end
 
-  def cas(message, params) do
-    params = params ++ [cas: slice_and_sum(message, 16,8)]
-  end
-
   def opcode(message, params) do
     opcode = case Enum.at(message, 1) do
       0x00 -> :get
@@ -49,6 +45,10 @@ defmodule Memcachedx.Packet.Response.Header do
       params = params ++ [total_body: Enum.slice(message, 8, 4) |> Enum.reduce(&+/2)]
     end
     params
+  end
+
+  def cas(message, params) do
+    params = params ++ [cas: slice_and_sum(message, 16,8)]
   end
 
   def merge_res(message, params) do
