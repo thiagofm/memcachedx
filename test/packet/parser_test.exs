ba14d3a31f07710ab92506f0d0afcf9c31fbed84
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-19 12:31:40 -0300

    Refactored parser

--- test/packet/parser_test.exs
+++ test/packet/parser_test.exs
@@ -1,26 +1,27 @@
 defmodule Memcachedx.Packet.ParserTest do
   use ExUnit.Case
+  alias Memcachedx.Packet.Parser, as: Parser
 
   test 'response add' do
-    assert Memcachedx.Packet.Parser.response([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == {:ok, [cas: 1, opcode: :add]}
+    assert Parser.response([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == {:ok, [cas: 1, opcode: :add]}
   end
 
   test 'status ok' do
-    assert Memcachedx.Packet.Parser.status([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :ok
+    assert Parser.status([129, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :ok
   end
 
   test 'status error' do
-    assert Memcachedx.Packet.Parser.status([129, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :error
+    assert Parser.status([129, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]) == :error
   end
 
   test :total_body do
-    assert Memcachedx.Packet.Parser.total_body([
+    assert Parser.total_body([
       0,0,0,0,
       0,0,0,0,
       0,0,0,0
     ], []) == []
 
-    assert Memcachedx.Packet.Parser.total_body([
+    assert Parser.total_body([
       0,0,0,0,
       0,0,0,0,
       0,0,0,1
@@ -28,25 +29,25 @@ defmodule Memcachedx.Packet.ParserTest do
   end
 
   test :body_parser do
-    assert Memcachedx.Packet.Parser.body_parser(
+    assert Parser.body_parser(
       [0, 0, 0, 0, 0, 0, 0, 3] , [total_body: 8, opcode: :incr]
     ) == [value: <<0,0,0,0,0,0,0,3>>]
   end
 
   test :body do
-    assert Memcachedx.Packet.Parser.body(
+    assert Parser.body(
       [129, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
    0, 0, 0, 0, 0, 3], [total_body: 8, opcode: :incr]
     )
   end
 
   test :extra_vars_for do
-    assert Memcachedx.Packet.Parser.extra_vars_for(:incr) == [:value]
-    assert Memcachedx.Packet.Parser.extra_vars_for(:decr) == [:value]
+    assert Parser.extra_vars_for(:incr) == [:value]
+    assert Parser.extra_vars_for(:decr) == [:value]
   end
 
   test :opcode do
-    assert Memcachedx.Packet.Parser.opcode([0, 0x01], []) == [opcode: :set]
-    assert Memcachedx.Packet.Parser.opcode([0, 0x16], []) == [opcode: :decrq]
+    assert Parser.opcode([0, 0x01], []) == [opcode: :set]
+    assert Parser.opcode([0, 0x16], []) == [opcode: :decrq]
   end
 end
