c40f1e39777e244cb7508ab93562c2d3326f89f7
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-19 12:08:19 -0300

    Added aliases for connection

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -1,5 +1,6 @@
 defmodule Memcachedx.ConnectionTest do
   use ExUnit.Case
+  alias Memcachedx.Connection, as: Connection
 
   setup do
     Memcachedx.TestHelper.memcached_up
@@ -12,59 +13,59 @@ defmodule Memcachedx.ConnectionTest do
   end
 
   test :start_link do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     assert pid != nil
   end
 
   test :stop do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     assert Memcachedx.Connection.stop(pid) == :ok
   end
 
   test :tcp_closed do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     Memcachedx.TestHelper.memcached_down
 
-    assert { :noproc, _ } = catch_exit( Memcachedx.Connection.stop(pid) )
+    assert { :noproc, _ } = catch_exit( Connection.stop(pid) )
   end
 
   test 'add from doc example' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:add, [key: "Hello", value: "World", cas: 0, flags: 0xdeadbeef, expiry: 0x00000e10]]) == {:ok, [cas: 1, opcode: :add]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:add, [key: "Hello", value: "World", cas: 0, flags: 0xdeadbeef, expiry: 0x00000e10]]) == {:ok, [cas: 1, opcode: :add]}
   end
 
   test 'add' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:add, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :add]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:add, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :add]}
   end
 
   test 'set from doc example' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :set]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :set]}
   end
 
   test 'set' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :set]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [cas: 1, opcode: :set]}
   end
 
   test 'delete success' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    Memcachedx.Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0, opcode: :set]])
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:delete, [key: "Hello"]]) == {:ok, [cas: 0, opcode: :delete]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0, opcode: :set]])
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:ok, [cas: 0, opcode: :delete]}
   end
 
   test 'delete fail' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:delete, [key: "Hello"]]) == {:error, [cas: 0, opcode: :delete, total_body: 9, value: "Not found"]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:error, [cas: 0, opcode: :delete, total_body: 9, value: "Not found"]}
   end
 
   test 'incr success' do
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    Memcachedx.Connection.run(pid, [:set, [key: "counter", value: "1", flags: 0, expiry: 0]])
-    {:ok, pid} = Memcachedx.Connection.start_link([hostname: "localhost", port: 11211])
-    assert Memcachedx.Connection.run(pid, [:incr, [key: "counter", delta: 0x01, initial: 0x00, expiration: 0x00000e10]]) == {:ok, [cas: 2, opcode: :incr, total_body: 8, value: <<0, 0, 0, 0, 0, 0, 0, 2>>]}
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    Connection.run(pid, [:set, [key: "counter", value: "1", flags: 0, expiry: 0]])
+    {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
+    assert Connection.run(pid, [:incr, [key: "counter", delta: 0x01, initial: 0x00, expiration: 0x00000e10]]) == {:ok, [cas: 2, opcode: :incr, total_body: 8, value: <<0, 0, 0, 0, 0, 0, 0, 2>>]}
   end
 
   test 'incr when key doesn\'t exist' do
