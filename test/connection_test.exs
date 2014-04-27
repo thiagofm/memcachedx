e29bc09d3975c484db12b455ee4d0a218e5ae086
Thiago Fernandes Massa <thiagown@gmail.com>
2014-04-26 23:08:10 -0300

    Changed most of the old api to binary

--- test/connection_test.exs
+++ test/connection_test.exs
@@ -31,60 +31,60 @@ defmodule Memcachedx.ConnectionTest do
 
   test 'add from doc example' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:add, [key: "Hello", value: "World", cas: 0, flags: 0xdeadbeef, expiry: 0x00000e10]]) == {:ok, [opcode: :add, key_length: 0, extra_length: 0, total_body: 0, cas: 1]}
+    assert Connection.run(pid, [:add, [key: "Hello", value: "World", cas: 0, flags: 0xdeadbeef, expiry: 0x00000e10]]) == {:ok, [opcode: :add, key_length: 0, extras_length: 0, total_body_length: 0, opaque: 0, cas: 1]}
   end
 
   test 'add' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:add, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :add, key_length: 0, extra_length: 0, total_body: 0, cas: 1]}
+    assert Connection.run(pid, [:add, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :add, key_length: 0, extras_length: 0, total_body_length: 0, opaque: 0, cas: 1]}
   end
 
   test 'set from doc example' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :set, key_length: 0, extra_length: 0, total_body: 0, cas: 1]}
+    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :set, key_length: 0, extras_length: 0, total_body_length: 0, opaque: 0, cas: 1]}
   end
 
   test 'set' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :set, key_length: 0, extra_length: 0, total_body: 0, cas: 1]}
+    assert Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]]) == {:ok, [opcode: :set, key_length: 0, extras_length: 0, total_body_length: 0, opaque: 0, cas: 1]}
   end
 
   test 'delete success' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     Connection.run(pid, [:set, [opcode: :set, key: "Hello", value: "World", flags: 0, expiry: 0]])
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:ok, [opcode: :delete, key_length: 0, extra_length: 0, total_body: 0, cas: 0]}
+    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:ok, [opcode: :delete, key_length: 0, extras_length: 0, total_body_length: 0, opaque: 0, cas: 0]}
   end
 
   test 'delete fail' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:error, [opcode: :delete, key_length: 0, extra_length: 0, total_body: 9, cas: 0, value: "Not found"]}
+    assert Connection.run(pid, [:delete, [key: "Hello"]]) == {:error, [opcode: :delete, key_length: 0, extras_length: 0, total_body_length: 9, opaque: 0, cas: 0, value: "Not found"]}
   end
 
   test 'incr success' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     Connection.run(pid, [:set, [key: "counter", value: "1", flags: 0, expiry: 0]])
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:incr, [key: "counter", delta: 0x01, initial: 0x00, expiration: 0x00000e10]]) == {:ok, [opcode: :incr, key_length: 0, extra_length: 0, total_body: 8, cas: 2, value: <<0, 0, 0, 0, 0, 0, 0, 2>>]}
+    assert Connection.run(pid, [:incr, [key: "counter", delta: 0x01, initial: 0x00, expiration: 0x00000e10]]) == {:ok, [opcode: :incr, key_length: 0, extras_length: 0, total_body_length: 8, opaque: 0, cas: 2, value: <<0, 0, 0, 0, 0, 0, 0, 2>>]}
     end
 
   test 'incr when key doesn\'t exist' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:incr, [key: "counter3", delta: 0x01, initial: 0x03, expiration: 0x00000e10]]) == {:ok, [opcode: :incr, key_length: 0, extras_length: 0, total_body: 8, cas: 1, opaque: 0, value: <<0, 0, 0, 0, 0, 0, 0, 3>>]}
+    assert Connection.run(pid, [:incr, [key: "counter3", delta: 0x01, initial: 0x03, expiration: 0x00000e10]]) == {:ok, [opcode: :incr, key_length: 0, extras_length: 0, total_body_length: 8, opaque: 0, cas: 1, value: <<0, 0, 0, 0, 0, 0, 0, 3>>]}
   end
 
   test 'get doc example' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     Connection.run(pid, [:set, [key: "Hello", value: "World", flags: 0, expiry: 0]])
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:get, [key: "Hello"]]) == {:ok, [opcode: :get, key_length: 0, extras_length: 4, total_body_length: 9, cas: 1, opaque: 0, flags: <<0, 0, 0, 0>>, value: "World"]}
+    assert Connection.run(pid, [:get, [key: "Hello"]]) == {:ok, [opcode: :get, key_length: 0, extras_length: 4, total_body_length: 9, opaque: 0, cas: 1, extras: 0, value: "World"]}
   end
 
   test 'get no defined flags' do
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
     Connection.run(pid, [:set, [key: "Hello", value: "World", expiry: 0]])
     {:ok, pid} = Connection.start_link([hostname: "localhost", port: 11211])
-    assert Connection.run(pid, [:get, [key: "Hello"]]) == {:ok, [opcode: :get, key_length: 0, extras_length: 4, total_body_length: 9, opaque: 0, cas: 1, extras: <<0, 0, 0, 0>>, value: "World"]}
+    assert Connection.run(pid, [:get, [key: "Hello"]]) == {:ok, [opcode: :get, key_length: 0, extras_length: 4, total_body_length: 9, opaque: 0, cas: 1, extras: 0, value: "World"]}
   end
 
   test 'get fail from doc example' do
