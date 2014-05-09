bc0d3dde59e1860ddc7159d6dd3dabb9799bd891
Thiago Fernandes Massa <thiagown@gmail.com>
2014-05-08 21:59:12 -0300

    Fixed travis.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,18 +1,20 @@
 defmodule Memcachedx.TestHelper do
   def memcached_up do
-    #System.cmd("sudo service memcached start")
-    System.cmd("memcached &")
-    sleep
+    if System.get_env("TEST_ENV") == "travis" do
+      System.cmd("sudo service memcached start")
+    else
+      System.cmd("memcached &")
+    end
+
+    :timer.sleep 50
   end
 
   def memcached_down do
-    System.cmd("pkill memcached")
-    #System.cmd("sudo pkill memcached")
-  end
-
-  # Sleep to wait for memcached process to be set up
-  defp sleep do
-    :timer.sleep 50
+    if System.get_env("TEST_ENV") == "travis" do
+      System.cmd("sudo pkill memcached")
+    else
+      System.cmd("pkill memcached")
+    end
   end
 end
 
