fe3eae82c2c8152386bb8e100aa301478d3f84aa
Thiago Fernandes Massa <thiagown@gmail.com>
2014-05-12 23:01:07 -0300

    Fixed infinite recursion with set

--- lib/command.ex
+++ lib/command.ex
@@ -16,16 +16,16 @@ defmodule Memcachedx.Command do
     end
   end
 
-  def set!(pid, key, value) do
-    case set!(pid, key, value) do
-      [{:ok, params}] -> true
-      [{:erro, params}] -> false
-    end
-  end
-
   def set(pid, key, value) do
     case Memcachedx.Connection.run(pid, [:set, [key: key, value: value]]) do
       res -> res
+    end
+  end
+
+  def set!(pid, key, value) do
+    case set(pid, key, value) do
+      [{:ok, params}] -> true
+      [{:error, params}] -> false
     end
   end
 end
