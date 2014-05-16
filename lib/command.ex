195416329caa08292b4236ed3fe80e1709fe311a
Thiago Fernandes Massa <thiagown@gmail.com>
2014-05-16 00:11:48 -0300

    Added docs for command

--- lib/command.ex
+++ lib/command.ex
@@ -1,4 +1,11 @@
 defmodule Memcachedx.Command do
+  @moduledoc """
+  Executes a command in memcached
+  """
+
+  @doc """
+  Gets the value of a key in memcached
+  """
   def get!(pid, key) do
     case get(pid, key) do
       [{:ok, params}] -> params[:value]
@@ -6,18 +13,27 @@ defmodule Memcachedx.Command do
     end
   end
 
+  @doc """
+  Gets the value of a key in memcached
+  """
   def get(pid, key) do
     case Memcachedx.Connection.run(pid, [:get, [key: key]]) do
       res -> res
     end
   end
 
+  @doc """
+  Sets a new key/value pair in memcached
+  """
   def set(pid, key, value) do
     case Memcachedx.Connection.run(pid, [:set, [key: key, value: value]]) do
       res -> res
     end
   end
 
+  @doc """
+  Sets a new key/value pair in memcached
+  """
   def set!(pid, key, value) do
     case set(pid, key, value) do
       [{:ok, params}] -> true
